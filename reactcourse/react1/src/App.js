f6b76aed25ab628a3e60b36b5465ba31069317f4
matheus <mbscbsjbs@gmail.com>
2021-05-28 15:15:57 -0300

    props

--- reactcourse/react1/src/App.js
+++ reactcourse/react1/src/App.js
@@ -10,21 +10,33 @@ function BemVindo(props) {
   return <h1>Olá, {props.nome}!</h1>
 }
 
+function Avatar(props) {
+  return (
+    <>
+      <img src={props.url} alt={props.name} />
+      <spam>{props.name}</spam>
+    </>
+  )
+}
+
 function App() {
 
-  // let usuario = {
-  //   nome:'Matheus',
-  //   idade: 21,
-  // }
+  let usuario = {
+    nome:'Matheus',
+    idade: 21,
+  }
 
   let imagem = 'https://www.google.com/google.jpg';
 
   return <div>
-    {/* <div>Meu nome é {usuario.nome} e tenho {usuario.idade} anos</div> */}
+    <div>Meu nome é {usuario.nome} e tenho {usuario.idade} anos</div>
     <img src={imagem} />
     <BemVindo nome="Matheus" />
     <BemVindo nome="João" />
     <BemVindo nome="Maria" />
+
+    <Avatar url={imagem} name="Google" />
+
   </div>
 };
 
