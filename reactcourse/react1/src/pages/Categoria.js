44b5bdd92b270bb3dd8a3ddb00d1a904529929b9
matheus <mbscbsjbs@gmail.com>
2021-05-30 13:14:21 -0300

    categorias

--- reactcourse/react1/src/pages/Categoria.js
+++ reactcourse/react1/src/pages/Categoria.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function Categoria() {
-  let { cat } = useParams();
+  let query = useQuery();
+
+  let cat = query.get('tipo');
+  let subtipo = query.get('subtipo');
 
   return (
     <div>
       <h4>Página CATEGORIA</h4>
-      Exibindo itens da categoria: {cat}
+      Exibindo itens da categoria: {cat} - {subtipo}
     </div>
   );
 }
