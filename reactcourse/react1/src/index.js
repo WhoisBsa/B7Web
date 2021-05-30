7d9affe5d1757b0396566a684dba48418f4074bf
matheus <mbscbsjbs@gmail.com>
2021-05-30 17:17:53 -0300

    combine reducers

--- reactcourse/react1/src/index.js
+++ reactcourse/react1/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElemestorentById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
