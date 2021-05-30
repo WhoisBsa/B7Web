99ed015e8508754e9f6d69ed5b20e23e5a69dcdb
matheus <mbscbsjbs@gmail.com>
2021-05-30 18:02:52 -0300

    redux hooks

--- reactcourse/react1/src/pages/Home.js
+++ reactcourse/react1/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-function Home(props) {
+function Home() {
   let history = useHistory();
+
+  const name = useSelector(state => state.usuario.name);
+  const contador = useSelector(state => state.usuario.contador);
+  const dispatch = useDispatch();
 
   const handleButton = () => {
     setTimeout(() => history.replace('/sobre'), 2000);
@@ -13,12 +17,20 @@ function Home(props) {
     <div>
       <h4>Página HOME</h4>
 
-      NOME: {props.name}<br />
-      CONTAGEM: {props.contador}
+      NOME: {name}<br />
+      CONTAGEM: {contador}
 
       <br /><br />
-      <button onClick={() => props.setName('Matheus')}>Setar nome para Matheus</button>
-      <button onClick={() => props.increment()}>+1</button>
+      <button 
+        onClick={() => dispatch({ type: 'SET_NAME', payload: { name: 'Matheus' } })}
+      >
+          Setar nome para Matheus
+      </button>
+      <button 
+        onClick={(() => dispatch({ type: 'SET_CONTADOR' }))}
+      >
+        +1
+      </button>
       <br /><br />
 
       <button onClick={handleButton}>Ir para página SOBRE</button>
@@ -26,17 +38,4 @@ function Home(props) {
   );
 }
 
-const mapStateToProps = (state) => (
-  {
-    name: state.usuario.name,
-    contador: state.usuario.contador,
-  }
-);
-
-const mapDispatchToProps = (dispatch) => (
-  {
-    setName: (name) => dispatch({ type: 'SET_NAME', payload: { name } }),
-    increment: () => dispatch({ type: 'SET_CONTADOR'})
-  }
-)
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
