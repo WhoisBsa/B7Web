09a92a7b01469d5fba0d375b25fda3a438381dc3
matheus <mbscbsjbs@gmail.com>
2021-05-30 17:14:54 -0300

    user reducer

---
+++ reactcourse/react1/src/reducers/UserReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  name: '',
+};
+
+const UsuarioReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return { ...state, name: action.payload.name };
+  }
+
+  return state;
+};
+
+export default UsuarioReducer;
